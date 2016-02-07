5f7d12c328390ed32edde7f297eec74c4f647933
Krzysiek102 <Krzysztof.Kurek102@gmail.com>
2016-02-07 20:58:57 +0100

    Jasmine async 2.0 compatible

--- jasmineSUT.js
+++ jasmineSUT.js
@@ -9,3 +9,11 @@ Calculator.prototype.add = function  (a, b){
 Calculator.prototype.divide = function  (a, b){
     $(this.el).html(a/b);
 }
+
+var CalculatorAsync = function  (displayElement){
+    this.$el = $(displayElement);
+};
+
+CalculatorAsync.prototype.hideResult = function  (cb){
+    this.$el.fadeOut(1000,cb);
+}
--- jasmineSUTSpec.js
+++ jasmineSUTSpec.js
@@ -1,3 +1,4 @@
+/* global AsyncSpec */
 describe('Calculator', function () {
     var calc;
     var OutputId = "#calc-fixture";
@@ -18,6 +19,29 @@ describe('Calculator', function () {
     it('should be able to divice 6 and 2', function () {
         calc.divide(6, 2);
         expect($(OutputId).text()).toBe("3");
+    });
+});
+
+describe('CalculatorAsync', function () {
+    var calc;
+    var el;
+
+    beforeEach(function () {
+        el = $("<div>some content</div>");
+        $("#container").append(el);
+        calc = new CalculatorAsync(el);
+    });
+    
+    afterEach(function  (){
+        el.remove();
+    });
+    
+    it('should work with a visual efect', function (done) {
+        var callback = function(){
+          expect(el.css('display')).toBe('none');
+          done();
+        };
+        calc.hideResult(callback);
     });
 });
     
