e18abfaefcb3319704969b795791d2361566cb13
Krzysiek102 <Krzysztof.Kurek102@gmail.com>
2016-02-07 13:55:19 +0100

    Asynchronous tests

--- systemundertest.js
+++ systemundertest.js
@@ -2,3 +2,7 @@ var SUT = {};
 SUT.CreateToDoItem = function(){
     $("#div1").append("<div class='js-todoContainer'> other html content</div>");
 }
+
+SUT.fadeOutDiv = function  (duration, callback){
+    $("#div1").fadeOut(duration, callback);
+}
--- test.js
+++ test.js
@@ -1,16 +1,53 @@
+/* global SUT */
 module('module 1');
 
-test('my first test', function(){
-   ok(true); 
+test('my first test', function () {
+    ok(true);
 });
 
 module('module 2');
 
-test('my second test', function(){
-   ok(true); 
+test('my second test', function () {
+    ok(true);
 });
 
-test('CreateToDoItem create 1 element', function  (){
+test('CreateToDoItem create 1 element', function () {
     SUT.CreateToDoItem();
     strictEqual($(".js-todoContainer").length, 1);
 });
+
+test('broken asynchronous timint test', function () {
+    stop();
+    setTimeout(function () {
+        ok(true);
+        start();
+    }, 100);
+})
+
+test('broken asynchronous timint test 2', function () {
+    stop(2);
+    setTimeout(function () {
+        ok(true);
+        console.log('longer timeout finished');
+        start();
+    }, 100);
+
+    setTimeout(function () {
+        ok(true);
+        start();
+    }, 100);
+})
+
+asyncTest('better asynchronous test', function () {
+    setTimeout(function () {
+        ok(true);
+        start();
+    }, 100);
+})
+
+asyncTest('UI asynchronous test', function () {
+    SUT.fadeOutDiv(500, function () {
+        ok(!$("#div1").is(":visible"));
+        start();
+    });
+})
