95b04c53cca6d2a1eda30b75d6961243c8b753ee
Krzysiek102 <Krzysztof.Kurek102@gmail.com>
2016-02-14 16:05:55 +0100

    Mocha asynchronous tests

--- mochaSUTSpec.js
+++ mochaSUTSpec.js
@@ -1,6 +1,38 @@
-suite('my first suite', function(){
-   test('test 1', function  (){
+mocha.setup({timeout:3000});
+describe('my first suite', function(){
+   
+   beforeEach(function  (){
+       console.log('beforeEach');
+       
+   })
+   
+   afterEach(function  (){
+       console.log('afterEach');
+   })
+   
+   it('test 1', function  (){
        chai.expect(1).to.equal(1);
+       console.log('test');
    }) 
+   
+   describe('inner suite', function  (){
+       it('test 2', function  (){
+           chai.expect(2).to.equal(2);
+           console.log('test 2');
+       })
+   })
 });
+
+
+describe('Outer Describe', function() {
+    it('should be asynchronous', function(done) {
+        this.timeout(4000);
+        setTimeout(function() {
+            chai.expect(1).to.equal(1);
+            done();
+        }, 3500);
+    });
+        
+});
+    
     
