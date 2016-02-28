6b4f6fc9db301df77544bf0f6ac82ef9b9f23486
Krzysiek102 <Krzysztof.Kurek102@gmail.com>
2016-02-28 14:12:06 +0100

    Sinon mocks

--- sinonSpec.js
+++ sinonSpec.js
@@ -34,5 +34,21 @@ describe('combat attack', function() {
         expect(defender.takeDamage.called).toBe(true);
         expect(defender.takeDamage.getCall(0).calledWith(5)).toBe(true);
     });
+    
+    it('should damage the defender if the hit is successfull - mock', function() {
+        var combat = new Combat();
+        var defender = new Character();
+        var mockDefender = sinon.mock(defender);
+        var expectation = mockDefender.expects("takeDamage").once().withArgs(5);
+        
+        var attacker = sinon.stub(new Character());
+        attacker.damage = 5;
+        attacker.calculateHit.returns(true);
+        combat.attack(attacker, defender);
+        
+        expectation.verify();
+    });
+        
 });
+    
     
